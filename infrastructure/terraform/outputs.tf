1a5498a1a3a54accd642c3495f581d08ef7f576b
sassenagh <anna.alcaide.hijano@gmail.com>
2026-03-18 19:54:46 +0100

    remove outputs

--- infrastructure/terraform/outputs.tf
+++
@@ -1,9 +0,0 @@
-data "aws_region" "current" {}
-
-output "aws_region" {
-  value = data.aws_region.current.name
-}
-
-output "aws_provider_region" {
-  value = var.aws_region != null ? var.aws_region : "NO_DEFINED"
-}
