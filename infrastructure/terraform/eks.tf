542bc96470e5c634041fc2728b4789918a34bde8
sassenagh <anna.alcaide.hijano@gmail.com>
2026-03-18 19:09:28 +0100

    fix eks name

--- infrastructure/terraform/eks.tf
+++ infrastructure/terraform/eks.tf
@@ -2,8 +2,8 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "21.15.1"
 
-  cluster_name    = var.cluster_name
-  cluster_version = "1.29"
+  name               = var.cluster_name
+  kubernetes_version = "1.29"
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
