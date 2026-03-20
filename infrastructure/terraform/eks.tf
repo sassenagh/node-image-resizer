3915170c5e1116093dbb8023f6f5aade28a82ffe
sassenagh <anna.alcaide.hijano@gmail.com>
2026-03-20 09:54:53 +0100

    update k8s version

--- infrastructure/terraform/eks.tf
+++ infrastructure/terraform/eks.tf
@@ -3,7 +3,7 @@ module "eks" {
   version = "21.15.1"
 
   name               = var.cluster_name
-  kubernetes_version = "1.29"
+  kubernetes_version = "1.33"
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
