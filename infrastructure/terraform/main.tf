06baba799bddb0d4249bdf43c51aa10c1bf31dee
sassenagh <anna.alcaide.hijano@gmail.com>
2026-03-20 13:14:42 +0100

    update vpc

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -11,4 +11,8 @@ module "vpc" {
   azs             = ["us-east-1a", "us-east-1b"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+
+  enable_nat_gateway     = true
+  single_nat_gateway     = true
+  one_nat_gateway_per_az = false
 }
