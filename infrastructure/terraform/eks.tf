e4a42da339d6200eb1443c2c62d18d2a55976d2f
sassenagh <anna.alcaide.hijano@gmail.com>
2026-03-23 12:03:27 +0100

    update eks

--- infrastructure/terraform/eks.tf
+++ infrastructure/terraform/eks.tf
@@ -1,9 +1,14 @@
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "21.15.1"
+  version = "~> 20.0"
 
   name               = var.cluster_name
   kubernetes_version = "1.33"
+
+  cluster_create_endpoint_access = true
+  cluster_endpoint_public_access_cidrs = ["0.0.0.0/0"]
+  
+  enable_irsa = true
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
